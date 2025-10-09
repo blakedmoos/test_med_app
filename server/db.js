852a7606adb1e62a296e3546ee1cf74b192509bb
blakedmoos <blakedmoos@gmail.com>
2025-10-09 11:38:35 -0400

    db functionality

--- server/db.js
+++ server/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoURI =  "mongodb://root:<your-password>@127.0.0.1:27017";
+const mongoURI =  "mongodb://root:Z75xMmXxTUeovgUCEkiBTJ0d@172.21.1.168:27017";
 
 const connectToMongo = async (retryCount) => {
     const MAX_RETRIES = 3;
